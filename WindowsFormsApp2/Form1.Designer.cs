5d7af483f37f1124ef6b4be9974fa1dc5786ea9b
Mini Pekka <ffgostkill@gmail.com>
2024-12-09 22:42:38 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 369);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ToolStripMenuItem menuItemFile = new ToolStripMenuItem("Файл");
+
+            ToolStripMenuItem menuItemSave = new ToolStripMenuItem("Сохранить");
+            menuItemSave.ShortcutKeys = Keys.Control | Keys.S;
+            menuItemSave.Click += new EventHandler(menuItemSave_Click);
+
+            ToolStripMenuItem menuItemOpen = new ToolStripMenuItem("Открыть");
+            menuItemOpen.ShortcutKeys = Keys.Control | Keys.A;
+            menuItemOpen.Click += new EventHandler(menuItemOpen_Click);
+
+            ToolStripMenuItem menuItemNew = new ToolStripMenuItem("Новый файл");
+            menuItemNew.ShortcutKeys = Keys.Control | Keys.D;
+            menuItemNew.Click += new EventHandler(menuItemNew_Click);
+
+            ToolStripMenuItem menuItemCalk = new ToolStripMenuItem("Калькулятор");
+            menuItemCalk.ShortcutKeys = Keys.Control | Keys.B;
+            menuItemCalk.Click += new EventHandler(menuItemCalculator_Click);
+
+
+            menuStrip1.Items.Add(menuItemFile);
+
+
+            menuItemFile.DropDownItems.Add(menuItemSave);
+            menuItemFile.DropDownItems.Add(menuItemOpen);
+            menuItemFile.DropDownItems.Add(menuItemNew);
+            menuItemFile.DropDownItems.Add(menuItemCalk);
+       
+            ToolStripMenuItem menuItemEdit = new ToolStripMenuItem("Редактировать");
+
+
+            ToolStripMenuItem menuItemCopy = new ToolStripMenuItem("Копировать");
+            menuItemCopy.ShortcutKeys = Keys.Control | Keys.C;
+            menuItemCopy.Click += new EventHandler(menuItemCopy_Click);
+
+            ToolStripMenuItem menuItemCut = new ToolStripMenuItem("Вырезать");
+            menuItemCut.ShortcutKeys = Keys.Control | Keys.X;
+            menuItemCut.Click += new EventHandler(menuItemCut_Click);
+
+            ToolStripMenuItem menuItemPaste = new ToolStripMenuItem("Вставить");
+            menuItemPaste.ShortcutKeys = Keys.Control | Keys.V;
+            menuItemPaste.Click += new EventHandler(menuItemPaste_Click);
+
+
+            menuStrip1.Items.Add(menuItemEdit);
+
+  
+            ToolStripMenuItem menuItemFont = new ToolStripMenuItem("Шрифт");
+            menuItemFont.Click += new EventHandler(menuItemFont_Click);
+
+            ToolStripMenuItem menuItemColor = new ToolStripMenuItem("Цвет");
+            menuItemColor.Click += new EventHandler(menuItemColor_Click);
+
+            menuStrip1.Items.Add(menuItemFont);
+            menuStrip1.Items.Add(menuItemColor);
+
+     
+            this.Controls.Add(menuStrip1);
+
+     
+            menuItemEdit.DropDownItems.Add(menuItemCopy);
+            menuItemEdit.DropDownItems.Add(menuItemCut);
+            menuItemEdit.DropDownItems.Add(menuItemPaste);
+            menuItemEdit.DropDownItems.Add(menuItemFont);
+            menuItemEdit.DropDownItems.Add(menuItemColor);
+
+
+        }
+
+     
+        private void menuItemCopy_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectedText.Length > 0)
+            {
+                richTextBox1.Copy();
+            }
+            else
+            {
+                MessageBox.Show("Нет выделенного текста для копирования!");
+            }
+        }
+
+
+        private void menuItemCut_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectedText.Length > 0)
+            {
+                richTextBox1.Cut();
+            }
+            else
+            {
+                MessageBox.Show("Нет выделенного текста для вырезания!");
+            }
+        }
+
+
+        private void menuItemPaste_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                richTextBox1.Paste();
+            }
+            else
+            {
+                MessageBox.Show("Буфер обмена пуст!");
+            }
+        }
+
+
+        private void menuItemFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Font = fontDialog.Font;
+            }
+        }
+
+
+        private void menuItemColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.ForeColor = colorDialog.Color;
+            }
+        }
+
+
+        private void menuItemOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.ShowDialog();
+                richTextBox1.LoadFile(openFileDialog1.FileName);
+            }
+            catch
+            {
+                MessageBox.Show("Файл не выбран!");
+            }
+        }
+
+
+        private void menuItemSave_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            if (!string.IsNullOrEmpty(saveFileDialog1.FileName))
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName);
+            }
+        }
+
+
+        private void menuItemNew_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void menuItemCalculator_Click(object sender, EventArgs e)
+        {
+
+            string calculatorPath = @"C:\Users\Arman\source\repos\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\calkulate.exe";
+
+            try
+            {
+                System.Diagnostics.Process.Start(calculatorPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при запуске калькулятора: {ex.Message}");
+            }
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
